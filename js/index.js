d45b44f0db7fac715ac7ccfb6f2c969e21895103
johnjanieltr <troyajohn17@gmail.com>
2024-04-01 14:06:54 -0400

    integrating form validation

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 import { changeMainMenu, toggleMainNavMobile } from "./MainNav.js";
 import { changeReviewCard, reviewCardFunction } from "./ReviewCards.js";
 
+const $newsletterFormInput = document.getElementById("newsletter-form-input"),
+  $newsletterFormErrorMsg = document.getElementById("newsletter-form-err-msg");
+
 let changeReviewCardsIsRunning;
 
-document.addEventListener("DOMContentLoaded", () => changeMainMenu());
+document.addEventListener("DOMContentLoaded", changeMainMenu);
 
 window.addEventListener("load", () => {
   if (!window.matchMedia("(min-width: 576px)").matches) {
@@ -16,6 +19,36 @@ document.addEventListener("click", (e) => {
   if (e.target.matches("#burger-btn")) toggleMainNavMobile();
   if (e.target.matches("#darkened-screen")) toggleMainNavMobile();
   if (e.target.matches(".main-nav__item a")) toggleMainNavMobile();
+});
+
+document.addEventListener("submit", (e) => {
+  if (e.target.matches("#newsletter-form")) {
+    e.preventDefault();
+    const REGEXP =
+      /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+    let inputValue = $newsletterFormInput.value;
+
+    if (REGEXP.test(inputValue)) {
+      $newsletterFormInput.value = "";
+      $newsletterFormInput.classList.remove("footer__form-email--err");
+      $newsletterFormInput.blur();
+      $newsletterFormErrorMsg.classList.add("hidden");
+    } else {
+      $newsletterFormInput.classList.add("footer__form-email--err");
+      $newsletterFormErrorMsg.classList.remove("hidden");
+    }
+  }
+});
+
+document.addEventListener("keypress", (e) => {
+  if (e.target.matches("#newsletter-form-input")) {
+    if (!$newsletterFormErrorMsg.classList.contains("hidden")) {
+      $newsletterFormInput.classList.remove("footer__form-email--err");
+      $newsletterFormErrorMsg.classList.add("hidden");
+    } else {
+      return;
+    }
+  }
 });
 
 window.addEventListener("resize", () => {
