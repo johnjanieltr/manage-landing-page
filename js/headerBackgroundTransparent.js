ee4706ffc49fbf7f4d240d8daed8d3c65c7d8084
johnjanieltr <troyajohn17@gmail.com>
2024-04-18 15:29:29 -0400

    improving the structure and design of the btns

--- js/headerBackgroundTransparent.js
+++ js/headerBackgroundTransparent.js
@@ -1,10 +1,12 @@
-const $header = document.getElementById("header");
+const $header = document.getElementById("header"),
+  $btnShadowHeader = document.querySelector(".btn--header .btn__shadow");
 
 const headerBackgroundTransparent = (e) => {
   if (window.scrollY < 15) {
     // header background -> transparent
     if (!$header.classList.contains("header--bg-transparent")) {
       $header.classList.add("header--bg-transparent");
+      $btnShadowHeader.classList.remove("btn__shadow-is-hidden");
     } else {
       return;
     }
@@ -12,6 +14,7 @@ const headerBackgroundTransparent = (e) => {
     // header background -> not transparent
     if ($header.classList.contains("header--bg-transparent")) {
       $header.classList.remove("header--bg-transparent");
+      $btnShadowHeader.classList.add("btn__shadow-is-hidden");
     } else {
       return;
     }
