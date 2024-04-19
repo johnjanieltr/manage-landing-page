87f7dffe29e87ff611034cb4fc5e2b93252de1fb
johnjanieltr <troyajohn17@gmail.com>
2024-04-19 12:10:03 -0400

    fixed issue with review cards

--- js/ReviewCards.js
+++ js/ReviewCards.js
@@ -1,13 +1,12 @@
 let $reviewCards = document.querySelectorAll(".review-card"),
   $reviewCardsNav = document.querySelectorAll(".rc-nav__li");
-
 $reviewCards = Array.from($reviewCards);
 $reviewCardsNav = Array.from($reviewCardsNav);
 
-export let changeReviewCard;
-
 let currentCard = 0,
   lastCard = 3;
+
+export let changeReviewCard;
 
 export const reviewCardFunction = () => {
   changeReviewCard = setInterval(() => {
@@ -15,19 +14,12 @@ export const reviewCardFunction = () => {
     if (currentCard >= $reviewCardsNav.length) currentCard = 0;
 
     $reviewCards[lastCard].classList.remove("review-cards--is-active");
-    $reviewCards[currentCard].classList.remove("hidden");
     $reviewCardsNav[lastCard].classList.remove("rc-nav__li--is-active");
     $reviewCardsNav[currentCard].classList.add("rc-nav__li--is-active");
-
     setTimeout(() => {
       $reviewCards[currentCard].classList.add("review-cards--is-active");
-    }, 0);
-    setTimeout(() => {
-      $reviewCards[lastCard].classList.add("hidden");
-    }, 800);
-    setTimeout(() => {
       lastCard = currentCard;
       currentCard++;
-    }, 1000);
+    }, 0);
   }, 5500);
 };
